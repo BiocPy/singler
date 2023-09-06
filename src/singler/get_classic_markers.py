e0433dc7b9510deb597f4f299328c57df07b523e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-06 16:30:26 -0700

    yet more fixes.

--- src/singler/get_classic_markers.py
+++ src/singler/get_classic_markers.py
@@ -83,7 +83,13 @@ def get_classic_markers(
                 "number of columns of 'ref' should be equal to the length of the corresponding 'labels'"
             )
 
-        r, f = _clean_matrix(curref, features[i], assay_type, check_missing)
+        r, f = _clean_matrix(
+            curref, 
+            features[i], 
+            assay_type = assay_type, 
+            check_missing = check_missing, 
+            num_threads = num_threads
+        )
         tmp_ref.append(r)
         tmp_features.append(f)
 
