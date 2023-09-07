806135c25aac0b8c7b9a12a6bb163b6edfe12582
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-07 07:53:26 -0700

    Added some docstrings.

--- src/singler/build_single_reference.py
+++ src/singler/build_single_reference.py
@@ -2,10 +2,24 @@ from .InternalMarkers import InternalMarkers
 from . import cpphelpers as lib
 from .utils import _factorize
 from mattress import tatamize
-from numpy import int32
+from numpy import int32, ndarray
 
 
 class SinglePrebuiltReference:
+    """A prebuilt reference object, typically created by
+    :py:meth:`~singler.build_single_reference.build_single_reference`.
+    This is intended for advanced users only and should not be serialized.
+
+    Attributes:
+        labels (Sequence):
+            Sequence of unique label identifiers, usually strings.
+
+        features (Sequence):
+            Sequence of feature identifiers, usually strings.
+            This contains the universe of all features known to the reference,
+            not just the markers that are ultimately used for classification.
+    """
+
     def __init__(self, ptr, labels: Sequence, features: Sequence):
         self._ptr = ptr
         self._features = features
@@ -14,25 +28,56 @@ class SinglePrebuiltReference:
     def __del__(self):
         lib.free_single_reference(self._ptr)
 
-    def num_features(self):
+    def num_markers(self) -> int:
+        """
+        Returns:
+            int: Number of markers to be used for classification.
+        """
         return lib.get_nsubset_from_single_reference(self._ptr)
 
-    def num_labels(self):
+    def num_labels(self) -> int:
+        """
+        Returns:
+            int: Number of unique labels in this reference.
+        """
         return lib.get_nlabels_from_single_reference(self._ptr)
 
     @property
-    def features(self):
+    def features(self) -> Sequence:
+        """
+        Returns: 
+            Sequence: The universe of features known to this reference,
+            usually as strings.
+        """
         return self._features
 
     @property
-    def labels(self):
+    def labels(self) -> Sequence:
+        """
+        Returns: 
+            Sequence: Unique labels in this reference.
+        """
         return self._labels
 
-    def subset(self):
+    def markers(self, indices_only: bool = False) -> Union[ndarray, list]:
+        """
+        Args:
+            indices_only (bool): Whether to return the markers as indices
+                into :py:attr:`~features`, or as a list of feature identifiers.
+                
+        Returns:
+            list: List of feature identifiers for the markers, if ``indices_only = False``.
+
+            ndarray: Integer indices of features in ``features`` chosen as markers,
+            if ``indices_only = True``.
+        """
         nmarkers = self.num_features()
         buffer = ndarray(nmarkers, dtype=int32)
         lib.get_subset_from_single_reference(self._ptr, buffer);
-        return [self._features[i] for i in buffer]
+        if indices_only:
+            return buffer
+        else:
+            return [self._features[i] for i in buffer]
 
 
 def build_single_reference(
@@ -43,6 +88,35 @@ def build_single_reference(
     approximate: bool = True,
     num_threads: int = 1,
 ):
+    """Build a single reference dataset in preparation for classification.
+
+    Args:
+        ref: A matrix-like object where rows are features and columns are 
+            reference profiles. This should contain expression values;
+            normalized and transformed values are also acceptable as only
+            the ranking is used within this function.
+
+        labels (Sequence): Sequence of labels for each reference profile,
+            i.e., column in ``ref``.
+
+        features (Sequence): Sequence of identifiers for each feature,
+            i.e., row in ``ref``.
+
+        markers (dict[Any, dict[Any, Sequence]]):
+            Upregulated markers for each pairwise comparison between labels.
+            Specifically, ``markers[a][b]`` should be a sequence of features
+            that are upregulated in ``a`` compared to ``b``. All such features
+            should be present in ``features``, and all labels in ``labels``
+            should have keys in the inner and outer dictionaries.
+
+        approximate (bool):
+            Whether to use an approximate neighbor search to compute scores
+            during classification.
+
+        num_threads (int):
+            Number of threads to use for reference building.
+    """
+
     lablev, labind = _factorize(labels)
     mrk = InternalMarkers.from_dict(markers, lablev, features)
     mat_ptr = tatamize(ref)
--- src/singler/classify_single_reference.py
+++ src/singler/classify_single_reference.py
@@ -8,13 +8,47 @@ from . import cpphelpers as lib
 
 def classify_single_reference(
     test,
-    ref: SinglePrebuiltReference,
     features: Sequence,
+    ref: SinglePrebuiltReference,
     quantile: float = 0.8,
-    use_fine_tune: bool = true,
+    use_fine_tune: bool = True,
     fine_tune_threshold: float = 0.05,
     num_threads: int = 1,
 ):
+    """
+    Classify a test dataset against a reference by assigning labels 
+    from the latter to each column of the former using the SingleR algorithm.
+
+    Args:
+        test: A matrix-like object where each row is a feature and each column
+            is a test sample (usually a single cell), containing expression values.
+            Normalized and transformed expression values are also acceptable as only
+            the ranking is used within this function.
+
+        features (Sequence): Sequence of identifiers for each feature in the test
+            dataset, i.e., row in ``test``.
+
+        ref (SinglePrebuiltReference):
+            A pre-built reference created with
+            :py:meth:`~singler.build_single_reference.build_single_reference`.
+
+        quantile (float):
+            Quantile of the correlation distribution for computing the score for each label.
+            Larger values increase sensitivity of matches at the expense of 
+            similarity to the average behavior of each label.
+
+        use_fine_tune (bool):
+            Whether fine-tuning should be performed. This improves accuracy for distinguishing
+            between similar labels but requires more computational work.
+
+        fine_tune_threshold (float):
+            Maximum difference from the maximum correlation to use in fine-tuning.            
+            All labels above this threshold are used for another round of fine-tuning.
+
+        num_threads (int):
+            Number of threads to use during classification.
+    """
+
     nl = ref.num_labels()
     mat_ptr = tatamize(test)
     nc = mat_ptr.ncol()
@@ -34,9 +68,11 @@ def classify_single_reference(
     for i, x in enumerate(features):
         mapping[x] = i
 
-    ref_subset = ref.subset()
+    ref_subset = ref.markers(indices_only = True)
+    ref_features = ref.features
     subset = ndarray((len(ref_subset),), dtype = int32)
-    for i, x in enumerate(ref_subset):
+    for i, y in enumerate(ref_subset):
+        x = ref_features[y]
         if x not in mapping:
             raise KeyError("failed to find gene '" + str(x) + "' in the test dataset")
         subset[i] = mapping[x]
