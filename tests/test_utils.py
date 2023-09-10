b79a54e64ffe0b51f9d99ce0b7ea71cf2552e0bf
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-09 22:57:08 -0700

    Bugfix for matching with duplicate levels.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,8 +27,8 @@ def test_match():
 
     # Handles duplicate targets.
     x = [5, 1, 2, 3, 5, 6, 7, 7, 2, 1]
-    mm = _match(x, [1, 2, 3, 3, 5, 6, 7, 6])
-    assert list(mm) == [3, 0, 1, 2, 3, 4, 5, 5, 1, 0]
+    mm = _match(x, [1, 2, 3, 3, 5, 6, 1, 7, 6])
+    assert list(mm) == [4, 0, 1, 2, 4, 5, 7, 7, 1, 0]
 
 
 def test_intersect():
